7d905433629070685b3aaa0338a05a644131543a
Tim Courrejou <timcour@users.noreply.github.com>
2024-05-10 12:24:58 -0700

    Consistent script names

--- kustool-easy-patch.sh
+++ kustool-easy-patch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# TODO: Validation checks
+# - check for valid kustomization file
+# - check for valid kustomize build
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 POSITIONAL_ARGS=()
@@ -9,16 +13,31 @@ function help {
     if [ ! -z "${ERROR}" ]; then
         echo "Error: $1"
     fi
-    echo "Usage: $0 KUSTOMIZATION_PATH"
+    echo "Usage: $0 <kustomization-path> [flags...]"
     echo ""
-    echo "  KUSTOMIZATION_PATH - /path/to/kustomization.yaml"
-    echo "  -k --kind - Kubernetes resource .kind"
-    echo "  -n --name - Kubernetes resource .metadata.name"
-    echo "  -h --help - print this message and exit"
+    echo "  kustomization-path - /path/to/kustomization.yaml"
+    echo ""
+    echo "Flags:"
+    echo "  -k --kind      - Kubernetes resource .kind"
+    echo "  -n --name      - Kubernetes resource .metadata.name"
+    echo "  -w --write     - Write updated patch target to specified"
+    echo "                   kustomization.yaml"
+    echo "  --file-to-diff - bypass the interactive editor, use an already"
+    echo "                   edited file.  Useful for testing."
+    echo "  --debug        - print debug messages"
+    echo "  -h --help      - print this message and exit"
     echo ""
     echo "Example:"
     echo "  Auto-update (or add) a patch to Deployment named 'app-name'"
     echo "    $0 --kind Deployment --name app-name /path/to/kustomization.yaml"
+}
+
+function debug {
+    if [ -z "${DEBUG}" ]; then
+        return
+    fi
+
+    >&2 echo -e "DEBUG: $@"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -33,10 +52,24 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+    -w|--write)
+      WRITE_KUSTOMIZATION_UPDATE=$2
+      shift
+      shift
+      ;;
+    --debug)
+      DEBUG=1
+      shift
+      ;;
     -h|--HELP)
       HELP="$2"
       help
       exit 0
+      ;;
+    --file-to-diff)
+      FILE_TO_DIFF="$2"
+      shift;
+      shift;
       ;;
     -*|--*)
       help "Unknown option $1"
@@ -77,6 +110,8 @@ if [[ ! -z "${NAME}" ]]; then
     QUERY_PARTS="$QUERY_PARTS .metadata.name==\"${NAME}\""
 fi
 RESOURCE_SELECT=$(join_by ' and ' $QUERY_PARTS)
+debug "RESOURCE_SELECT yq selection query"
+debug "${RESOURCE_SELECT}"
 
 QUERY_PARTS=""
 if [[ ! -z "${KIND}" ]]; then
@@ -100,8 +135,8 @@ function delete_patches {
 function push_unpatched_kustomize {
     BAKFILE=${1:-"${KUST_FILE}.bak"}
     mv "${KUST_FILE}" "${BAKFILE}"
-    echo "writing unpatched kustomization"
-    cat "${BAKFILE}" | delete_patches
+    debug "writing unpatched kustomization"
+    debug $(cat "${BAKFILE}" | delete_patches)
     cat "${BAKFILE}" | delete_patches > "${KUST_DIR}"/kustomization.yaml
 }
 
@@ -110,8 +145,9 @@ function pop_unpatched_kustomize {
     mv  "${BAKFILE}" "${KUST_FILE}"
 }
 
+debug "Filtering kustomize build resources: ${KUST_FILE}; select: ${RESOURCE_SELECT}"
 # only one document at a time supported
-RESOURCE_COUNT=$("${SCRIPT_DIR}"/kustomize-select-resource.sh -c "${KUST_FILE}" -s "${RESOURCE_SELECT}")
+RESOURCE_COUNT=$("${SCRIPT_DIR}"/kustool-filter-resource.sh -c "${KUST_FILE}" -s "${RESOURCE_SELECT}")
 if [[ ${RESOURCE_COUNT} -gt 1 ]]; then
     echo "Only one supported at this time.  Ensure --kind and --name are"
     echo " specified."
@@ -121,18 +157,22 @@ fi
 #TODO: also ensure _at most_ one patch matches
 
 # Build and write current fully _patched_ yaml to EDIT_FILE
-"${SCRIPT_DIR}"/kustomize-select-resource.sh "${KUST_FILE}" -s "${RESOURCE_SELECT}" > ${EDIT_FILE}
+"${SCRIPT_DIR}"/kustool-filter-resource.sh "${KUST_FILE}" -s "${RESOURCE_SELECT}" > ${EDIT_FILE}
 
 # Remove patches, build and write _unpatched_ yaml to UNPATCHED_BUILD_FILE
 push_unpatched_kustomize "${KUST_FILE}.bak"
-"${SCRIPT_DIR}"/kustomize-select-resource.sh "${KUST_FILE}" -s "${RESOURCE_SELECT}" > ${UNPATCHED_BUILD_FILE}
+"${SCRIPT_DIR}"/kustool-filter-resource.sh "${KUST_FILE}" -s "${RESOURCE_SELECT}" > ${UNPATCHED_BUILD_FILE}
 pop_unpatched_kustomize "${KUST_FILE}.bak"
 
 # edit EDIT_FILE
 # TODO: why does quoting the tempfile not work?
-"${EDITOR:-vi}" ${EDIT_FILE}
+if [ ! -z $FILE_TO_DIFF ]; then
+    cat "${FILE_TO_DIFF}" > ${EDIT_FILE}
+else
+    "${EDITOR:-vi}" ${EDIT_FILE}
+fi
 
-if ! cat ${EDIT_FILE} | yq; then
+if ! cat ${EDIT_FILE} | yq > /dev/null; then
     echo "Error parsing yaml:"
     cat -n ${EDIT_FILE}
     echo ""
@@ -144,8 +184,8 @@ fi
 export PATCHES="$(jd -f patch -yaml ${UNPATCHED_BUILD_FILE} ${EDIT_FILE} | yq -P -o yaml)"
 
 #TODO: only echo of -p specified
-echo "patches:"
-echo "${PATCHES}"
+debug "Generated updated yamlfied JSON patch"
+debug "${PATCHES}"
 
 #TODO: insert (or replace) the single patch matching PATCHES_SELECT _using_ the .kind/.metadata.name from the yaml resource.
 
@@ -161,63 +201,32 @@ if [[ ! -z "${NAME}" ]]; then
 fi
 # yq '.patches[] | select(.target.kind== "Deployment")'
 export NOMUTATE_SELECT="$(join_by ' or ' $QUERY_PARTS)"
-echo "NOMUTATE_SELECT: $NOMUTATE_SELECT"
 
-##### How do i grab the array elements, not the damn string!?
+debug "Filter select query for other patch targets: $NOMUTATE_SELECT"
 
-# Filter all non-targeted resources out
-echo "PATCHES_SELECT"
-echo "$PATCHES_SELECT"
 
 CURRENT_PATCH_TARGET=$(cat "${KUST_FILE}" | yq  '.patches | filter(eval(strenv(PATCHES_SELECT)))')
-echo "CURRENT_PATCH_TARGET"
-echo "${CURRENT_PATCH_TARGET}"
+debug "CURRENT_PATCH_TARGET\n"${CURRENT_PATCH_TARGET}""
 
-#TODO: factor out yq queries for readability, e.g.
-# function replace_patches {
-#      yq -o json '.[0].patch = strenv(PATCHES)'
-# }
-
-echo "generating new patch"
 export NEW_PATCH_TARGET=$(echo "${CURRENT_PATCH_TARGET}" | yq  '.[0].patch |= strenv(PATCHES)')
-echo -e --- NEW_PATCH_TARGET
-echo "${NEW_PATCH_TARGET}"
-
-
-QUERY_PARTS=""
-if [[ ! -z "${KIND}" ]]; then
-    QUERY_PARTS="\"kind\":\"${KIND}\""
-fi
-if [[ ! -z "${NAME}" ]]; then
-    QUERY_PARTS="$QUERY_PARTS \"name\":\"${NAME}\""
-fi
-# yq '.patches[] | select(.target.kind== "Deployment")'
-export PATCH_SELECTOR=$(join_by ' and ' $QUERY_PARTS)
-
-echo "PATCH_SELECTOR"
-echo "$PATCH_SELECTOR"
+debug "The patched patch target"
+debug "${NEW_PATCH_TARGET}"
 
 function replace_patch_target {
     export SELECT="${1}"
     export PATCH_TARGET="${2}"
-    # echo "SELECT: $SELECT"
-    # echo "PATCH_TARGET: $PATCH_TARGET"
-    yq '.patches |= filter(eval(strenv(SELECT))) += strenv(PATCH_TARGET)'
+    yq '.patches |= filter(eval(strenv(SELECT))) *+ env(PATCH_TARGET)'
 }
 
-# Remove the single patch target from the original, and append the new
-echo "without function"
-cat "${KUST_FILE}" | yq '.patches |= filter(eval(strenv(NOMUTATE_SELECT))) *+ env(NEW_PATCH_TARGET)'
-echo "without function"
+# # Remove the single patch target from the original, and append the new
+# debug "without function"
+# cat "${KUST_FILE}" | yq '.patches |= filter(eval(strenv(NOMUTATE_SELECT))) *+ env(NEW_PATCH_TARGET)'
 
-# echo "with the function"
-# cat "${KUST_FILE}" | replace_patch_target "${NOMUTATE_SELECT}" "${NEW_PATCH_TARGET}"
+debug "Generating kustomization with updated patch target"
+NEW_KUSTOMIZATION=$(cat "${KUST_FILE}" | replace_patch_target "${NOMUTATE_SELECT}" "${NEW_PATCH_TARGET}")
+echo "${NEW_KUSTOMIZATION}"
 
-
-
-
-
-# Append to array
-#cat ${KUST_FILE} | yq e '.patches += eval(strenv(PATCHES))'
-
-# ORIG_FILE=$(mktemp)
+if [ ! -z ${WRITE_KUSTOMIZATION_UPDATE} ]; then
+    echo "${NEW_KUSTOMIZATION}" > ${KUST_FILE}
+    debug "Updated ${KUST_FILE}"
+fi
